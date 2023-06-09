70236787c20e11a29e9abb0297c2e1333d9a8a8e
janardan <janardonhazarika7@gmail.com>
2023-06-10 03:24:21 +0530

    working, js was loading before html file, so no divs created, soln: use defer in script tag

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const parentDiv = document.querySelector(".parent-div");
 
 for (var i = 0; i < 10; i++) {
-
     var childDiv = document.createElement("div");
-    childDiv.setAttribute("class","row-div");
     childDiv.style.display = "flex";
     childDiv.style.flex = "1";
 
     for (var k = 0; k < 10; k++) {
         var newChild = document.createElement("div");
-        newChild.setAttribute("class", "child-div");
+        newChild.setAttribute("class","child-div");
+
         childDiv.appendChild(newChild);
     }
 
